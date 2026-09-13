9e9046abcdbdbe57c8bd6b95a9a4c88aca8ac111
wickwirez-beep <wickwirez-beep@users.noreply.github.com>
2026-09-12 21:09:29 -0500

    Remove duplicate imports

--- app/src/main/java/com/wickwirez/mailwarden/MainActivity.kt
+++ app/src/main/java/com/wickwirez/mailwarden/MainActivity.kt
@@ -25,16 +25,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.ui.draw.clip
 import androidx.compose.material3.AssistChip
-import androidx.compose.foundation.background
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.ui.draw.clip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
